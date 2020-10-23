af0aa78157d54ee03bbb84b9873878fa61a945ad
gokulkrishh <krishnangokul9@gmail.com>
2020-10-23 21:08:42 +0530

    Update npx card

--- build.js
+++ build.js
@@ -1,48 +1,46 @@
-'use strict';
+'use strict'
 
 // Pull in our modules
-const chalk = require('chalk');
-const boxen = require('boxen');
-const fs = require('fs');
-const path = require('path');
+const chalk = require('chalk')
+const boxen = require('boxen')
+const fs = require('fs')
+const path = require('path')
 
 // Define options for Boxen
 const options = {
   padding: 1,
   margin: 1,
-  borderStyle: 'round',
-};
+  borderStyle: 'round'
+}
 
 // Text + chalk definitions
 const data = {
-  name: chalk.white('Gokulakrishnan Kalaikovan'),
+  name: chalk.green(' Gokulakrishnan Kalaikovan'),
   handle: chalk.white('gokulkrishh'),
-  work: chalk.white('Web Dev @ Thoughtworks'),
-  twitter: chalk.cyan('https://twitter.com/') + chalk.cyan('gokul_i'),
-  npm: chalk.cyan('https://npmjs.com/') + chalk.red('~gokulkrishh'),
-  github: chalk.cyan('https://github.com/') + chalk.green('gokulkrishh'),
-  web: chalk.cyan('https://gokul.site'),
-  dribble: chalk.cyan('https://dribbble.com/gokulkrishh'),
-  npx: chalk.red('npx') + ' ' + chalk.white('gokulkrishh'),
-  labelWork: chalk.white.bold('       Work:'),
-  labelTwitter: chalk.white.bold('    Twitter:'),
-  labelnpm: chalk.white.bold('        npm:'),
-  labelGitHub: chalk.white.bold('     GitHub:'),
-  labelWeb: chalk.white.bold('       Blog:'),
-  labelDribble: chalk.white.bold('    Dribble:'),
-  labelCard: chalk.white.bold('       Card:'),
-};
+  work: chalk.blueBright('Web Developer at ThoughtWorks'),
+  twitter: chalk.underline.magenta('https://twitter.com/') + chalk.underline.magenta('gokul_i'),
+  npm: chalk.underline.magenta('https://npmjs.com/') + chalk.underline.magenta('~gokulkrishh'),
+  github: chalk.underline.magenta('https://github.com/') + chalk.underline.magenta('gokulkrishh'),
+  web: chalk.underline.magenta('https://gokul.site'),
+  dribble: chalk.underline.magenta('https://dribbble.com/gokulkrishh'),
+  npx: chalk.yellow('npx') + ' ' + chalk.yellow('gokulkrishh'),
+  labelWork: chalk.white.bold('     Work:'),
+  labelTwitter: chalk.white.bold('  Twitter:'),
+  labelnpm: chalk.white.bold('      npm:'),
+  labelGitHub: chalk.white.bold('   GitHub:'),
+  labelWeb: chalk.white.bold('     Blog:'),
+  labelDribble: chalk.white.bold('  Dribble:'),
+  labelCard: chalk.white.bold('     Card:')
+}
 
 // Actual strings we're going to output
-const newline = '\n';
-const heading = `${data.name} / ${data.handle}`;
-const working = `${data.labelWork}  ${data.work}`;
-const twittering = `${data.labelTwitter}  ${data.twitter}`;
-const npming = `${data.labelnpm}  ${data.npm}`;
-const githubing = `${data.labelGitHub}  ${data.github}`;
-const webing = `${data.labelWeb}  ${data.web}`;
-const dribble = `${data.labelDribble}  ${data.dribble}`;
-const carding = `${data.labelCard}  ${data.npx}`;
+const newline = '\n'
+const heading = `${data.name} `
+const working = `${data.labelWork}  ${data.work} `
+const twittering = `${data.labelTwitter}  ${data.twitter} `
+const githubing = `${data.labelGitHub}  ${data.github} `
+const webing = `${data.labelWeb}  ${data.web} `
+const carding = `${data.labelCard}  ${data.npx} `
 
 // Put all our output together into a single variable so we can use boxen effectively
 const output =
@@ -51,18 +49,14 @@ const output =
   newline + // Add one whole blank line
   working +
   newline + // data.labelWork + data.work
-  newline + // data.labelOpenSource + data.opensource
-  twittering +
-  newline + // data.labelTwitter + data.twitter
-  npming +
-  newline + // data.labelnpm + data.npm
-  githubing +
-  newline +
-  dribble +
   newline +
   webing +
   newline +
+  twittering +
+  newline + // data.labelnpm + data.npm
+  githubing +
   newline +
-  carding; // data.labelCard + data.npx
+  newline +
+  carding // data.labelCard + data.npx
 
-fs.writeFileSync(path.join(__dirname, 'bin/output'), chalk.green(boxen(output, options)));
+fs.writeFileSync(path.join(__dirname, 'bin/output'), chalk.green(boxen(output, options)))
